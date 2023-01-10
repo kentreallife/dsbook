1f9532dc01d9d73efba3c12904e3f65b0fe49456
kentreallife <kentreallife@gmail.com>
2023-01-10 19:46:19 +0900

    test

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,30 +1,31 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
- 
+
 # アクセストークン（先ほど発行されたアクセストークンに書き換えてください）
-TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
- 
- 
+TOKEN = "5424829187:AAGw9BgIeaux1OkbjS3KLI1T7VSKvpvkPo0"
+
+
 class TelegramBot:
     def __init__(self, system):
         self.system = system
- 
+
     def start(self, bot, update):
         # 辞書型 inputにユーザIDを設定
         input = {'utt': None, 'sessionId': str(update.message.from_user.id)}
- 
+
         # システムからの最初の発話をinitial_messageから取得し，送信
         update.message.reply_text(self.system.initial_message(input)["utt"])
- 
+
     def message(self, bot, update):
         # 辞書型 inputにユーザからの発話とユーザIDを設定
-        input = {'utt': update.message.text, 'sessionId': str(update.message.from_user.id)}
- 
+        input = {'utt': update.message.text,
+                 'sessionId': str(update.message.from_user.id)}
+
         # replyメソッドによりinputから発話を生成
         system_output = self.system.reply(input)
- 
+
         # 発話を送信
         update.message.reply_text(system_output["utt"])
- 
+
     def run(self):
         updater = Updater(TOKEN)
         dp = updater.dispatcher
@@ -32,4 +33,3 @@ class TelegramBot:
         dp.add_handler(MessageHandler(Filters.text, self.message))
         updater.start_polling()
         updater.idle()
-        
